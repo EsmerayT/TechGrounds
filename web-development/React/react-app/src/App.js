a9b5ebd22b830cd873c92997a6ba3cebd20903a9
Esmeray Tasgit <estasgit@gmail.com>
2021-02-17 11:56:47 +0100

    React App Sectie 3

--- web-development/React/react-app/src/App.js
+++ web-development/React/react-app/src/App.js
@@ -1,41 +1,46 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-  state = {
+const app = props => {
+  const [ personsState, setPersonsState ] = useState({
     persons: [
       {name: 'Max', age: 28},
       {name: 'Manu', age: 29},
       {name: 'Esmeray', age: 30}
-    ],
-    otherState: 'Some other value'
-  }
+    ]
+  });
 
-  switchNameHandler = () => {
-    this.setState({
+  const [otherState, setOtherState]= useState('Some other value');
+
+  console.log(personsState, otherState);
+
+  const switchNameHandler = () => {
+    setPersonsState({
       persons: [
         {name: 'Maximilian', age: 28},
         {name: 'Manu', age: 30},
         {name: 'Esmeray', age: 30}
-      ]
+      ],
+      otherState: personsState.otherState
+    });
+  };
 
-    })
-  }
-
-  render() {
     return (
       <div className="App">
         <h1>Hi, I am a React App!</h1>
         <p>This is realy working</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >My hobbies: Racing</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+        <button onClick={switchNameHandler}>Switch Name</button>
+        <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
+        <Person name={personsState.persons[1].name} age={personsState.persons[1].age} >My hobbies: Racing</Person>
+        <Person name={personsState.persons[2].name} age={personsState.persons[2].age} />
       </div>
     );
   }
-}
 
-export default App;
+
+export default app;
+
+
+
 
