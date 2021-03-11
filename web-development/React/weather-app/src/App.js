e64ded414c3aba50840e82efdddc6fea6971039a
Esmeray Tasgit <estasgit@gmail.com>
2021-03-11 13:52:17 +0100

    Weather Component

--- web-development/React/weather-app/src/App.js
+++ web-development/React/weather-app/src/App.js
@@ -7,86 +7,97 @@ window.jQuery = $;
 window.$ = $;
 global.jQuery = $;
 
-let i = 0;
-const city = ["Dordrecht", "Bali"];
+// let i = 0;
+// const city = ["Dordrecht", "Bali"];
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      city: undefined,
-      // country: undefined,
-      icon: undefined,
-      main: undefined,
-      temp: undefined,
-      temp_max: undefined,
-      temp_min: undefined,
-      humidity: undefined,
-      error: false
-    };
-    this.getWeather();
-  }
-
-  getWeather = async () => {
-
-    const api_call = await fetch(
-      // `http://api.openweathermap.org/data/2.5/group?id=1650535,2756669&cnt=2&units=metric&lang=nl&appid=${process.env.REACT_APP_WEATHER_API_KEY}`
-      `http://api.openweathermap.org/data/2.5/weather?q=${city[i++]}&units=metric&lang=nl&appid=${process.env.REACT_APP_WEATHER_API_KEY}`
-    );
-
-    // 1650535,2756669
-
-    // for (i = 0; i < city.length; i++) {
-    //   console.log(city)
-    // }
+// class App extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       city: undefined,
+//       // country: undefined,
+//       icon: undefined,
+//       main: undefined,
+//       temp: undefined,
+//       temp_max: undefined,
+//       temp_min: undefined,
+//       humidity: undefined,
+//       error: false
+//     };
+//     this.getWeather();
+//   }
 
 
-    const response = await api_call.json();
+//   getWeather = async () => {
+
+//     const api_call = await fetch(
+//       // `http://api.openweathermap.org/data/2.5/group?id=1650535,2756669&cnt=2&units=metric&lang=nl&appid=${process.env.REACT_APP_WEATHER_API_KEY}`
+//       `http://api.openweathermap.org/data/2.5/weather?q=${city[i++]}&units=metric&lang=nl&appid=${process.env.REACT_APP_WEATHER_API_KEY}`
+//     );
+
+   
+
+//     const response = await api_call.json();
 
   
 
-    console.log(response);
-    // console.log(city[0]);
-    // console.log(city[1]);
+//     console.log(response);
+   
 
 
-    this.setState({
-      city: response.name,
-      // country: response.sys.country,
-      temp: response.main.temp,
-      temp_min: response.main.temp_min,
-      temp_max: response.main.temp_max,
-      humidity: response.main.humidity,
-      icon: response.weather.icon,
-    })
-  };
+//     this.setState({
+//       city: response.name,
+//       // country: response.sys.country,
+//       temp: response.main.temp,
+//       temp_min: response.main.temp_min,
+//       temp_max: response.main.temp_max,
+//       humidity: response.main.humidity,
+//       icon: response.weather.icon,
+//     })
+//   };
 
 
-  render() {
-    return (
-      <div className="App">
-        <Weather
-          city={this.state.city}
-          // city="Dordrecht"
-          // country={this.state.country} 
-          temp={this.state.temp}
-          temp_max={this.state.temp_max}
-          temp_min={this.state.temp_min}
-          humidity={this.state.humidity}
-          icon={this.state.icon}
-        />
-        <Weather
-          city={this.state.city}
-          //  city="Bali"
-          // country={this.state.country} 
-          temp={this.state.temp}
-          temp_max={this.state.temp_max}
-          temp_min={this.state.temp_min}
-          humidity={this.state.humidity}
-          icon={this.state.icon} />
-      </div>
-    );
-  }
-}
+//   render() {
+//     return (
+//       <div className="App">
+//         <Weather
+//           city={this.state.city}
+//           city="Dordrecht"
+//           country={this.state.country} 
+//           temp={this.state.temp}
+//           temp_max={this.state.temp_max}
+//           temp_min={this.state.temp_min}
+//           humidity={this.state.humidity}
+//           icon={this.state.icon}
+//         />
+//         <Weather
+//           city={this.state.city}
+//            city="Bali"
+//           country={this.state.country} 
+//           temp={this.state.temp}
+//           temp_max={this.state.temp_max}
+//           temp_min={this.state.temp_min}
+//           humidity={this.state.humidity}
+//           icon={this.state.icon} />
+//       </div>
+//     )
+//   }
+// }
+
+
+// export default App;
+
+
+const App = () => {
+  return (
+    <div className="cards">
+      <Weather city="Dordrecht"/>
+      <Weather city="Bali"/>
+    </div>
+  );
+};
 
 export default App;
+
+
+
--- web-development/React/weather-app/src/Components/weather.jsx
+++ web-development/React/weather-app/src/Components/weather.jsx
@@ -1,39 +1,51 @@
 import React from 'react';
 
-const Weather = (props) => {
-    
-    return (
-        <div className="container">
-            <div className="cards">
-                <h1>
-                    {props.city}
-                    {/* {props.country} */}
-                </h1>
-                <h5 className="icons">
-                {/* <img src={`http://openweathermap.org/img/wn/${props.icon}@2x.png`} alt=""/> */}
-                    <img src="http://openweathermap.org/img/wn/02d@2x.png" alt="" />
-                </h5>
-                <h1 className="temp">{Math.floor(props.temp)}&deg;</h1>
-                {minmaxTemp(Math.floor(props.temp_min), Math.floor(props.temp_max))}
-                <h4>Vochtigheid</h4>
-                <h4 className="humidity">{props.humidity}%</h4>
+const Weather = ({ city }) => {
+
+
+    Weather = async () => {
+
+        const api_call = await fetch(
+            // `http://api.openweathermap.org/data/2.5/group?id=1650535,2756669&cnt=2&units=metric&lang=nl&appid=${process.env.REACT_APP_WEATHER_API_KEY}`
+            `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&lang=nl&appid=${process.env.REACT_APP_WEATHER_API_KEY}`
+        );
+
+
+        const response = await api_call.json();
+
+
+        console.log(response);
+
+
+        return (
+            <div className="container">
+                <div className="cards">
+                    <h1>
+                        {city}
+                        {/* {props.country} */}
+                    </h1>
+                    <h5 className="icons">
+                        {/* <img src={`http://openweathermap.org/img/wn/${props.icon}@2x.png`} alt=""/> */}
+                        <img src="http://openweathermap.org/img/wn/02d@2x.png" alt="" />
+                    </h5>
+                    <h1 className="temp">{Math.floor(main.temp)}&deg;</h1>
+                    {minmaxTemp(Math.floor(main.temp_min), Math.floor(main.temp_max))}
+                    <h4>Vochtigheid</h4>
+                    <h4 className="humidity">{main.humidity}%</h4>
+                </div>
             </div>
-        </div>
-    );
+        );
+    };
+
+    function minmaxTemp(min, max) {
+        return (
+            <h3>
+                <span>Min:{min}&deg;</span>&emsp;
+                <span>Max:{max}&deg;</span>
+            </h3>
+        );
+
+    };
 };
 
-function minmaxTemp(min, max) {
-    return (
-        <h3>
-            <span>Min:{min}&deg;</span>&emsp;
-            <span>Max:{max}&deg;</span>
-        </h3>
-    );
-
-}
-
-
-
 export default Weather;
-
-
