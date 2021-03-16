2e1cdd16d84dbe352cde52bd9b2c4bc8ae7aebd7
Esmeray Tasgit <estasgit@gmail.com>
2021-03-16 15:32:52 +0100

    Weather Detail

--- web-development/React/weather-api/src/App.js
+++ web-development/React/weather-api/src/App.js
@@ -12,16 +12,15 @@ import './App.css';
 
 
 function App() {
- 
+
   return (
     <div className="App">
-        {/* <WeatherCard
-          city="Dordrecht" />
-        <WeatherCard
-          city="Bali" />
-        <WeatherDetail /> */}
-        <WeatherDetail lat="51.81" lon="4.6736" />
-      </div>
+      {/* <WeatherCard
+        city="Dordrecht" />
+      <WeatherCard
+        city="Bali" /> */}
+      <WeatherDetail lat="51.81" lon="4.6736" />
+    </div>
   );
 }
 
--- web-development/React/weather-api/src/Components/WeatherDetail.jsx
+++ web-development/React/weather-api/src/Components/WeatherDetail.jsx
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
+let i = 0;
+
 const WeatherDetail = ({ lat, lon }) => {
-    const [details, setDetails] = useState({});
+    const [details, setDetails] = useState("");
+    const [date, setDate] = useState("");
     useEffect(() => {
         axios({
             method: "GET",
@@ -11,24 +14,68 @@ const WeatherDetail = ({ lat, lon }) => {
             .then((response) => {
                 console.log(response);
                 setDetails(response.data);
+                setDate(response.data.daily);
             })
             .catch((error) => console.log(error));
     }, [lat, lon]);
+    console.log(date);
 
-    const date = new Date(details.current.dt * 1000);
-    let newDate = date.toLocaleDateString();
+    // let d = new Date(date * 1000).toLocaleDateString();
+    // console.log(d)
+
+    const d = new Date(date * 1000);
+    let newDate = d.toLocaleDateString();
     console.log(newDate);
+    console.log(details)
+    
+
+
+    // const realDate = date.map((datum) =>
+    //     new Date(datum.dt * 1000).toLocaleDateString()
+    // );
+    // console.log(realDate);
+
+    let i = 0;
+
+    for (let i = 0; i < date.length; i++) {
+        console.log(date[i].dt);
+    }
+
+
+ 
+
+
+
+     // const forecast = new Date(date * 1000);
+    // let newDate = forecast.toLocaleDateString();
+    // console.log(newDate);
+    // console.log(details)
+
+
+
 
     return (
         <div className="details">
-            <p>{newDate}</p>
-            <img src={`http://openweathermap.org/img/wn/${details.daily[0].weather[0].icon}@2x.png`} alt="" />
-            <p>{Math.floor(details.daily[0].temp.min)}/{Math.floor(details.daily[0].temp.max)}&deg;</p>
-            <p>Temperature</p>
-            <p>Morning: {Math.floor(details.daily[0].temp.morn)}&deg;</p>
-            <p>Afternoon: {Math.floor(details.daily[0].temp.day)}&deg;</p>
-            <p>Evening: {Math.floor(details.daily[0].temp.eve)}&deg;</p>
-            <p>Night: {Math.floor(details.daily[0].temp.night)}&deg;</p>
+            {details && (
+                <div>
+                    {/* {
+                        <div>
+                            {realDate.map((datum) => (
+                                <div>{datum}</div>
+                            ))}
+                        </div>
+                    } */}
+                    <h1>Dordrecht</h1>
+                    <p>{newDate}</p>
+                    <img src={`http://openweathermap.org/img/wn/${details.daily[0].weather[0].icon}@2x.png`} alt="" />
+                    <p>{Math.floor(details.daily[0].temp.min)}/{Math.floor(details.daily[0].temp.max)}&deg;C</p>
+                    <p>Temperature</p>
+                    <p>Morning: {Math.floor(details.daily[0].temp.morn)}&deg;C</p>
+                    <p>Afternoon: {Math.floor(details.daily[0].temp.day)}&deg;C</p>
+                    <p>Evening: {Math.floor(details.daily[0].temp.eve)}&deg;C</p>
+                    <p>Night: {Math.floor(details.daily[0].temp.night)}&deg;C</p>
+                </div>
+            )}
         </div>
     );
 };
