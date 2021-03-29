399b19023596a8c369f202b9ff09096c8bea0c75
Esmeray Tasgit <estasgit@gmail.com>
2021-03-29 13:33:35 +0200

    Firebase Sign Up

--- web-development/React/react-firebase-authentication/src/components/SignOut/index.js
+++ web-development/React/react-firebase-authentication/src/components/SignOut/index.js
@@ -1,11 +1,11 @@
-import React from 'react'
-
-const SignOut = () => {
-    return (
-        <div>
-            <h1>Sign Out</h1>          
-        </div>
-    )
-}
-
-export default SignOut
+import React from 'react';
+ 
+import { withFirebase } from '../Firebase';
+ 
+const SignOutButton = ({ firebase }) => (
+  <button type="button" onClick={firebase.doSignOut}>
+    Sign Out
+  </button>
+);
+ 
+export default withFirebase(SignOutButton);
